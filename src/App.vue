8703ce73a7ddd46f597f94922e5eb1b0842cc907
Wormss9 <Wormss9.bd@gmail.com>
2022-03-06 19:41:14 +0100

    Fix header

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
         <router-link to="/whatson">What's on</router-link>
         <router-link to="/contacts">Contact</router-link>
       </Slide>
-      <router-link class="icon" to="/">
-        <img class="icon_img" src="./assets/logo.jpg" />
+      <router-link class="l" to="/">
+        <img class="icon" src="./assets/logo.jpg" />
       </router-link>
     </header>
     <router-view />
