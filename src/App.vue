ff148f0a18b8e0bad7b26442b14ee6628730f68c
Wormss9 <Wormss9.bd@gmail.com>
2022-03-06 16:08:52 +0100

    Enhance footer

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,25 @@
     </div>
     <router-view />
     <footer class="footer">
-      <router-link to="/contacts">Contact</router-link>
+      <div class="l">
+        <img class="icon l" src="./assets/icons/ig.png" />
+        <a
+          class="link"
+          href="https://www.instagram.com/parados.exodus/?utm_medium=copy_link&fbclid=IwAR23kbsBspJXvltY1_VosTJXVjrk-q382k6b7616h45aZKKe8G75N-Fcf2g"
+        >
+          parados.exodus
+        </a>
+      </div>
+      <router-link class="m" to="/contacts">Contact</router-link>
+      <div class="r">
+        <a
+          class="link"
+          href="https://www.facebook.com/Parados-Exodus-103039302184087/"
+        >
+          Parados,Exodus
+        </a>
+        <img class="icon r" src="./assets/icons/fb.png" />
+      </div>
     </footer>
   </div>
 </template>
